d334d3b097898e5656c3214608308c6b60a9649c
Saurav Sharma <80798063+Debugger-OP@users.noreply.github.com.>
2024-09-13 22:03:00 +0530

    2nd commit

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: "https://jobhuntproj.onrender.com",
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 
